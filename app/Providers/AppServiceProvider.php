91585ce537bea3b2a1f8026d67e4a78038ed6ba8
FarisLucky <luckyfaris44@gmail.com>
2022-09-06 22:13:57 +0700

    ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.env') == 'production') {
-            URL::forceScheme('https');
+		if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
         }
         Paginator::useBootstrap();
     }
