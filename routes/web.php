00c332b40d7b92caa1e4042f39d784943d6f316c
FarisLucky <luckyfaris44@gmail.com>
2022-09-06 22:00:11 +0700

    ssl

--- routes/web.php
+++ routes/web.php
@@ -48,14 +48,13 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-	'ssl',
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('home');
     })->name('dashboard');
 });
 
-Route::group(['middleware' => ['auth','ssl']], function () {
+Route::group(['middleware' => 'auth'], function () {
     Route::get('/kategori/data', [KategoriController::class, 'data'])
         ->name('kategori.data');
     Route::resource('/kategori', KategoriController::class);
