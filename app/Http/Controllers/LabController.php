6229f4fa15eeae21887f1f70eb19f2f334bab0c3
khizam 19 <achsanulkhizam19@gmail.com>
2022-09-10 18:55:54 +0700

    lab ganti

--- app/Http/Controllers/LabController.php
+++ app/Http/Controllers/LabController.php
@@ -170,7 +170,7 @@ class LabController extends Controller
     {
         try {
             $this->authorize('lab_edit');
-            $lab = Lab::find($id);
+            $lab = Lab::where('id_lab', $id)->first();
             if ($lab == null) {
                 throw new NotFoundHttpException("Barang tidak ditemukan");
             }
